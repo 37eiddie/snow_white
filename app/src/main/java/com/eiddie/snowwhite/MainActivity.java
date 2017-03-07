400bb1e33c5cf7a6dbe3e6c1b2171f9f8ee6a515
Eddie Jeon and Eileen Kwon <pair+hb.jeon+jiwon.kwon@samsung.com>
2017-03-07 13:20:12 +0900

    add Analog Clock, Text Clock, Date

--- app/src/main/java/com/eiddie/snowwhite/MainActivity.java
+++ app/src/main/java/com/eiddie/snowwhite/MainActivity.java
@@ -1,13 +1,31 @@
 package com.eiddie.snowwhite;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.tomerrosenfeld.customanalogclockview.CustomAnalogClock;
+
+import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LocalDate date = LocalDate.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern("MMM dd, E");
+
+        String str = date.toString(dateTimeFormatter);
+
+        TextView textView = (TextView) findViewById(R.id.text_date);
+        textView.setText(str);
+
+        CustomAnalogClock customAnalogClock = (CustomAnalogClock) findViewById(R.id.analog_clock);
+        customAnalogClock.setAutoUpdate(true);
     }
 }
