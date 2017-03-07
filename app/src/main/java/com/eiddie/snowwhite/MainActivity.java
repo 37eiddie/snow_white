6dedd112ba6d6685bd2727e002325e351af7f6e2
Eddie Jeon and Eileen Kwon <pair+hb.jeon+jiwon.kwon@samsung.com>
2017-03-07 14:09:32 +0900

    add rss reader

--- app/src/main/java/com/eiddie/snowwhite/MainActivity.java
+++ app/src/main/java/com/eiddie/snowwhite/MainActivity.java
@@ -2,6 +2,7 @@ package com.eiddie.snowwhite;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.tomerrosenfeld.customanalogclockview.CustomAnalogClock;
@@ -9,6 +10,9 @@ import com.tomerrosenfeld.customanalogclockview.CustomAnalogClock;
 import org.joda.time.LocalDate;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
+import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSReader;
+import org.mcsoxford.rss.RSSReaderException;
 
 public class MainActivity extends Activity {
 
@@ -27,5 +31,23 @@ public class MainActivity extends Activity {
 
         CustomAnalogClock customAnalogClock = (CustomAnalogClock) findViewById(R.id.analog_clock);
         customAnalogClock.setAutoUpdate(true);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        RSSReader reader = new RSSReader();
+
+        String uri = "https://news.google.com/news?cf=all&hl=ko&pz=1&ned=kr&output=rss";
+        try {
+            RSSFeed feed = reader.load(uri);
+
+            Log.d("feed",""+feed.getItems().size());
+
+        } catch (RSSReaderException e) {
+            e.printStackTrace();
+        }
+
     }
 }
